d1b27729c2a839fbdb169ad2f0e1914abf9c277f
tzhasansohan <sohantajbiul210@gmail.com>
2023-09-01 00:39:10 +0400

    for deploy

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
   images: {
     domains: ["lh3.googleusercontent.com"], // Add the hostname here
   },
+  swcMinify: true,
+  output: "export",
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+  eslint: { ignoreDuringBuilds: true },
+  i18n: {
+    locales: ["en", "es", "de"],
+    defaultLocale: "en",
+  },
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
 };
